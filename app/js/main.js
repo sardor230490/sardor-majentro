43048b0fc62e1d42780831edd2ce62c2c5d4c2fa
sardor230490 <sardor.ibragimov.1990@gmail.com>
2020-02-14 02:06:17 +0600

    slider add

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
 $(function(){
  var mixer = mixitup('.products__inner-box');
+ $(".rate__star").rateYo({
+  rating: 5,
+  starWidth: "12px", 
+  readOnly: true
+ 
+});
+$('.product__slider-inner').slick({
+   arrows: false,
+   dots:true,
+   slidesToShow: 4,
+   slidesToScroll: 4
 
-
-
+}); 
 });
