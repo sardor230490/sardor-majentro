d2bfd84a97e0567d825e45948ec19eba4ba06374
sardor230490 <sardor.ibragimov.1990@gmail.com>
2020-03-02 13:43:46 +0600

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -35,6 +35,12 @@ $('.icon-th-large').on('click', function () {
    $('.products__item').removeClass('list')
    $('.products__item').removeClass('active'); 
 });
- 
+ $('.menu__btn').on('click', function() {
+    $('.menu__list').slideToggle();
+ });
+ $('.header__btn-menu').on('click', function() {
+    $('.header__box').toggleClass('active');
+ });
+
 var mixer = mixitup('.products__inner-box');
 });
