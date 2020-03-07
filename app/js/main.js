fe3adf78d249f974e68cd1b64bdaf6f14ec65ebb
sardor230490 <sardor.ibragimov.1990@gmail.com>
2020-03-07 17:12:10 +0600

    setting page

--- app/js/main.js
+++ app/js/main.js
@@ -31,10 +31,10 @@ $('.icon-th-list').on('click', function () {
    $('.products__item').addClass('active')
 });
 
-$('.product__one-tabs .tab').on('click', function(event) {
+$('.product__one-tabs .tab, .tabs-settings .tab').on('click', function(event) {
    var id = $(this).attr('data-id');
-      $('.product__one-tabs').find('.tab-item').removeClass('active-tab').hide();
-      $('.product__one-tabs .tabs').find('.tab').removeClass('active');
+      $('.product__one-tabs, .tabs-settings').find('.tab-item').removeClass('active-tab').hide();
+      $('.product__one-tabs .tabs, .tabs-settings .tabs').find('.tab').removeClass('active');
       $(this).addClass('active');
       $('#'+id).addClass('active-tab').fadeIn();
       return false;
