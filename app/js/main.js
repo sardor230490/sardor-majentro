60d00a050f6095bc4a32ade1bb4dfc015d49a511
sardor230490 <sardor.ibragimov.1990@gmail.com>
2020-03-12 02:39:38 +0600

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,36 @@ $('.product__slider-inner').slick({
    arrows: false,
    dots:true,
    slidesToShow: 4,
-   slidesToScroll: 4
+   slidesToScroll: 4,
+   responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      }, 
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      }
+    ]
 
 });
  
